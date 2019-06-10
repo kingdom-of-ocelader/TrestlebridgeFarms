da9b24227887f6074fd7dd6c0dfc1d1ffc540abf
William Mitchell <wemitchell3@gmail.com>
2019-06-10 12:53:43 -0500

    Uploading duckhouse facility component

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Facilities;
@@ -7,7 +8,10 @@ namespace Trestlebridge.Actions {
     public class CreateFacility {
         public static void CollectInput (Farm farm) {
             Console.WriteLine ("1. Grazing field");
-            Console.WriteLine ("2. Plowed field");
+            Console.WriteLine ("2. Natural Field");
+            Console.WriteLine ("3. Plowed field");
+            Console.WriteLine ("4. Chicken House");
+            Console.WriteLine ("5. Duck House");
 
             Console.WriteLine ();
             Console.WriteLine ("Choose what you want to create");
@@ -18,7 +22,15 @@ namespace Trestlebridge.Actions {
             switch (Int32.Parse(input))
             {
                 case 1:
+                    Console.WriteLine("Congrats you added a grazing field");
+                    Thread.Sleep(3000);
                     farm.AddGrazingField(new GrazingField());
+                    break;
+                
+                case 5:
+                    Console.WriteLine("Congrats you added a duck house.");
+                    Thread.Sleep(3000);
+                    farm.AddDuckHouse(new DuckHouse());
                     break;
                 default:
                     break;

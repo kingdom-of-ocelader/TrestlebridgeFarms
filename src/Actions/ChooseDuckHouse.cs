7b192fa959006dd828cbeee602229ab278fb8424
unknown <cliftonmatuszewski@gmail.com>
2019-06-11 14:07:40 -0500

    Got chicken and ducks leaving a mesage saying congrats on buying an animal!

--- src/Actions/ChooseDuckHouse.cs
+++ src/Actions/ChooseDuckHouse.cs
@@ -1,27 +1,32 @@
 using System;
 using System.Linq;
+using System.Threading;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 
-namespace Trestlebridge.Actions {
-    public class ChooseDuckHouse {
-        public static void CollectInput (Farm farm, IDucks animal) {
+namespace Trestlebridge.Actions
+{
+    public class ChooseDuckHouse
+    {
+        public static void CollectInput(Farm farm, IDucks animal)
+        {
             Console.Clear();
 
             for (int i = 0; i < farm.DuckHouses.Count; i++)
             {
-                Console.WriteLine ($"{i + 1}. Number of ducks in duck house {i + 1}: {farm.DuckHouses[i].NumberOfAnimals}");
+                Console.WriteLine($"{i + 1}. Number of ducks in duck house {i + 1}: {farm.DuckHouses[i].NumberOfAnimals}");
             }
 
-            Console.WriteLine ();
+            Console.WriteLine();
 
             // How can I output the type of animal chosen here?
-            Console.WriteLine ($"Place the animal where?");
+            Console.WriteLine($"Place the animal where?");
 
-            Console.Write ("> ");
-            int choice = Int32.Parse(Console.ReadLine ());
-
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Congrats on buying a new animal!");
+            Thread.Sleep(1000);
             farm.DuckHouses[choice - 1].AddResource(animal);
 
             /*
